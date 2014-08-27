5541a20d6a7e79ae4787184c0122cb9c270e83bc
unknown <MOUTON-A@MOUTON-A.OCEANIE.local>
2014-08-27 17:31:26 +0200

    fix blanket coverage

--- test/index.js
+++ test/index.js
@@ -1,3 +1,13 @@
+var should = require('should');
+
 describe('Github hosting for node-pre-gyp', function(){
-	// TODO
+	var githubHosting = require('../index.js');
+	it('should define 3 functions', function(){
+		githubHosting.publish.should.be.type('function');
+		githubHosting.unpublish.should.be.type('function');
+		githubHosting.download.should.be.type('function');
+		githubHosting.publish({}, function(err){
+			should.not.exist(err);
+		});
+	});
 });
